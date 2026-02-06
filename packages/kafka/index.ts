c43e704b5de3d710232a203fa567a40bee85a80e
Rohit <rohitnarnolia88@gmail.com>
2026-02-07 04:39:52 +0530

    add: timeout for kafka

--- packages/kafka/index.ts
+++ packages/kafka/index.ts
@@ -7,4 +7,11 @@ const BROKER_URL = "localhost:9092";
 export const kafka = new Kafka({
   brokers: [BROKER_URL],
   clientId: CLIENT_ID,
+  requestTimeout: 30000,
+  connectionTimeout: 10000,
+  retry: {
+    retries: 8,
+    initialRetryTime: 100,
+    maxRetryTime: 30000,
+  },
 });
