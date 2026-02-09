c7fa24dfad34ba985453dddfb8a1a8b083cab811
Rohit <rohitnarnolia88@gmail.com>
2026-02-10 02:36:44 +0530

    chore: add enum type and zod schema for gemini node

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,5 +1,12 @@
 import { z } from "zod";
 
+export const AVAILABLE_MODELS = [
+  "gemini-flash-latest",
+  "gemini-pro-latest",
+  "gemini-2.5-flash",
+  "gemma-3-12b-it",
+] as const;
+
 export const httpSchema = z.object({
   name: z
     .string("Name is required")
@@ -14,5 +21,20 @@ export const httpSchema = z.object({
   method: z.enum(["GET", "POST", "PUT", "PATCH", "DELETE"]),
   body: z.string().optional(),
 });
+
+export const geminiSchema = z.object({
+  name: z
+    .string("Name is required")
+    .min(2, "Name must be at least 2 characters long")
+    .regex(
+      /^[A-Za-z_$][A-Za-z0-9_$]*$/,
+      "Name must start with a letter, underscore or dollar sign and can only contain letters, numbers, underscores or dollar signs",
+    ),
+  model: z.enum(AVAILABLE_MODELS),
+  systemPrompt: z.string().optional(),
+  userPrompt: z.string().min(1, "User prompt is required"),
+});
+
+export type GeminiNodeConfig = z.infer<typeof geminiSchema>;
 
 export type HttpNodeConfig = z.infer<typeof httpSchema>;
