39aaf18a32fa34898505be1522d89c731af4f93d
Rohit <rohitnarnolia88@gmail.com>
2026-02-11 05:39:17 +0530

    config changes

--- packages/config-eslint/library.js
+++ packages/config-eslint/library.js
@@ -4,15 +4,33 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "turbo"],
-  plugins: ["only-warn"],
+  root: true,
+
+  env: {
+    node: true,
+    browser: true,
+  },
+
+  parser: "@typescript-eslint/parser",
+
+  parserOptions: {
+    project,
+  },
+
+  plugins: ["@typescript-eslint", "only-warn"],
+
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "turbo",
+  ],
+
   globals: {
     React: true,
     JSX: true,
   },
-  env: {
-    node: true,
-  },
+
   settings: {
     "import/resolver": {
       typescript: {
@@ -20,12 +38,24 @@ module.exports = {
       },
     },
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
-  ],
+
+  ignorePatterns: [".*.js", "node_modules/", "dist/"],
+
+  rules: {
+    // Kill JS rules (they break TS)
+    "no-unused-vars": "off",
+    "no-undef": "off",
+    "no-redeclare": "off",
+
+    // Use TS-aware ones
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+    "@typescript-eslint/no-redeclare": [
+      "warn",
+      { ignoreDeclarationMerge: true },
+    ],
+  },
+
   overrides: [
     {
       files: ["*.js?(x)", "*.ts?(x)"],
--- packages/config-eslint/next.js
+++ packages/config-eslint/next.js
@@ -4,20 +4,32 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+
+  parser: "@typescript-eslint/parser",
+
+  parserOptions: {
+    project,
+  },
+
+  plugins: ["@typescript-eslint", "only-warn"],
+
   extends: [
     "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "prettier",
     require.resolve("@vercel/style-guide/eslint/next"),
     "turbo",
   ],
+
   globals: {
     React: true,
     JSX: true,
   },
-  env: {
-    node: true,
-  },
-  plugins: ["only-warn"],
+
   settings: {
     "import/resolver": {
       typescript: {
@@ -25,10 +37,23 @@ module.exports = {
       },
     },
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-  ],
+
+  ignorePatterns: ["*.js", "node_modules/"],
+
+  rules: {
+    // Disable JS rules
+    "no-unused-vars": "off",
+    "no-undef": "off",
+    "no-redeclare": "off",
+
+    // Enable TS rules
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+    "@typescript-eslint/no-redeclare": [
+      "warn",
+      { ignoreDeclarationMerge: true },
+    ],
+  },
+
   overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
 };
