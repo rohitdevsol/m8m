6fecd0fc222bb95f3cd76b2f1f620f9a6e16117e
Rohit <rohitnarnolia88@gmail.com>
2026-02-12 21:38:08 +0530

    add snapshot transformer function

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -1,4 +1,5 @@
 import Cryptr from "cryptr";
+import type { Node, Connection } from "@repo/database";
 
 const cryptr = new Cryptr(process.env.ENCRYPTION_KEY!);
 
@@ -9,3 +10,22 @@ export const encrypt = (text: string) => {
 export const decrypt = (text: string) => {
   return cryptr.decrypt(text);
 };
+
+export function snapshotWorkflow(nodes: Node[], connections: Connection[]) {
+  return {
+    nodes: nodes.map((n) => ({
+      id: n.id,
+      name: n.name,
+      type: n.type,
+      position: n.position,
+      data: n.data,
+    })),
+    connections: connections.map((c) => ({
+      id: c.id,
+      fromNodeId: c.fromNodeId,
+      toNodeId: c.toNodeId,
+      fromOutput: c.fromOutput,
+      toInput: c.toInput,
+    })),
+  };
+}
